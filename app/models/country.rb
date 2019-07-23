81cd58414767222d299d98da65bacb1f1ebb3f21
Maksym Lemich <lemichello.m@gmail.com>
2019-07-23 21:45:20 +0300

    fix

--- app/models/country.rb
+++ app/models/country.rb
@@ -11,7 +11,7 @@ class Country
 
     countries.each do |country|
       country['borders'].map! do |country_code|
-        countries.find { |country| country['alpha3Code'] == country_code }['name']
+        countries.find { |c| c['alpha3Code'] == country_code }['name']
       end
       country['languages'].map! { |language| language['name'] }
     end
