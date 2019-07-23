f4a9f5adde0748135c03265cdf1f9284c3ea6804
Maksym Lemich <lemichello.m@gmail.com>
2019-07-23 19:27:42 +0300

    Initial commit

---
+++ app/models/country.rb
@@ -0,0 +1,21 @@
+class Country
+  include HTTParty
+
+  default_options.update(verify: false)
+  base_uri 'https://restcountries.eu/rest/v2'
+
+  format :json
+
+  def self.all
+    countries = get('/all')
+
+    countries.each do |country|
+      country['borders'].map! do |country_code|
+        countries.find { |country| country['alpha3Code'] == country_code }['name']
+      end
+      country['languages'].map! { |language| language['name'] }
+    end
+
+    countries
+  end
+end
