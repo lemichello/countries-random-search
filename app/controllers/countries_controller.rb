06c38241e3fb84abfbfd5d7c966bd11de5ff369e
Maksym Lemich <lemichello.m@gmail.com>
2019-07-23 22:04:38 +0300

    refactoring

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -5,7 +5,7 @@ class CountriesController < ApplicationController
     limit = params[:limit].to_i
     limit = rand(5..25) if limit.zero?
 
-    @countries_to_display = all_countries.sample(limit)
-    @countries_to_display.sort! { |a, b| a['name'] <=> b['name'] }
+    @display_countries = all_countries.sample(limit)
+    @display_countries.sort! { |a, b| a['name'] <=> b['name'] }
   end
 end
